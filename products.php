305ea89acd688ead4bffc67722b37ed25fd6ad87
Alfaisal Eldajani <aeldajani@fb.com>
2021-10-28 15:45:16 +0300

    changing products page to laptops

--- products.php
+++ products.php
@@ -4,9 +4,9 @@
  </head>
  <body>
     <ul>
-        <li><a href="/product.php?<?php echo http_build_query(['id' => 1]) ?>">iPhone</a></li>
-        <li><a href="/product.php?id=2">Pixel Phone</a></li>
-        <li><a href="/product.php?id=3">Samsung</a></li>
+        <li><a href="/product.php?id=1">Alienware</a></li>
+        <li><a href="/product.php?id=2">Mabook Pro</a></li>
+        <li><a href="/product.php?id=3">Acer Predator</a></li>
     </ul>
  </body>
 </html>
