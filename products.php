b0be3968f1ef3af4a563b468384f7f191db149ac
Faisal <faisal@gmail.com>
2021-10-28 12:09:45 +0300

    First commit to add the products file

---
+++ products.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>My Online Store</title>
+ </head>
+ <body>
+    <ul>
+        <li><a href="/product1.php">iPhone</a></li>
+        <li><a href="/product2.php">Pixel Phone</a></li>
+        <li><a href="/product3.php">Samsung</a></li>
+    </ul>
+ </body>
+</html>
